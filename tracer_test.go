80b3315338c5e42765789ad143f3844966d3c761
James Muriuki <jamesmuriukimbuthia@gmail.com>
2019-02-10 21:14:06 +0000

    Add failing test New to test the trace New function

--- tracer_test.go
+++ tracer_test.go
@@ -1,9 +1,21 @@
 package trace
 
 import(
+	"bytes"
 	"testing"
+
 )
 
 func TestNew(t *testing.T){
-	t.Error("We have not written our test yet")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil{
+		t.Error("Return from New should not be nil")
+	}else{
+		tracer.Trace("Hello Trace package")
+		if buf.String() != "Hello Trace package\n"{
+			t.Errorf("Tracer should write '&s'.",buf.String())
+		}
+	}
 }
